59b116cec87f4a78dcff78212a608c1fc23278e4
trk907728 <108434139+trk907728@users.noreply.github.com>
2022-08-06 13:37:29 +0700

    Create popme_indo.js

---
+++ popme_indo.js
@@ -0,0 +1,42 @@
+var go_current     	= window.location.href;
+    var reff     		= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "https://t.co/rEumra0mYR", //==> direct link pipit
+                            "https://t.co/sVnUQpoPkQ",	//==> direct link omcik
+                        ]);
+
+    var ars             = rChoice([
+                            "#YOUR_ARSAE_SERVER_OR_OTHER_DOMAIN_1",
+                        ]);
+
+
+    var dir_type        = "domain"; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
