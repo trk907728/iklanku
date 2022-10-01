1140029a37ec15daa907f3cb2088df34e2f72b73
trk907728 <108434139+trk907728@users.noreply.github.com>
2022-10-02 04:48:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mapAds = ["/country.js", "/anti-adblock.js", "/detect-click.js"];
+const mapAds = ["/country.js", "/analytic.js", "/anti-adblock.js", "/detect-click.js"];
 var hostUri = "ads.soalcinta.my.id";
 const randomNumberHash = (n) => {
     for (var r = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], e = n, t = new Array, a = 0; a <= e - 1; a++) {
